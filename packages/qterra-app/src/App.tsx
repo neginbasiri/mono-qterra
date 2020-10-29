bdc22b5c0d71331c53ee760897c71ac5ef9abcc9
nbasiri <nbasiri@kpmg.com.au>
2020-10-29 15:29:37 +1100

    Added graphql and Apollo

--- packages/qterra-app/src/App.tsx
+++ packages/qterra-app/src/App.tsx
@@ -1,27 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-
 import { Button, Paper } from "@material-ui/core";
+
+const client = new ApolloClient({
+  uri: "",
+});
 
 function App() {
   return (
-    <Container maxWidth="sm" className="App">
-      <Paper>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App + Material-UI
-        </Typography>
-        <Button variant="contained" color="primary">
-          Primary Button
-        </Button>
-        <Button variant="contained" color="secondary">
-          Secondary Button
-        </Button>
-      </Paper>
-    </Container>
+    <ApolloProvider client={client}>
+      <Container maxWidth="sm" className="App">
+        <Paper>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Typography variant="h4" component="h1" gutterBottom>
+            Create React App + Material-UI
+          </Typography>
+          <Button variant="contained" color="primary">
+            Primary Button
+          </Button>
+          <Button variant="contained" color="secondary">
+            Secondary Button
+          </Button>
+        </Paper>
+      </Container>
+    </ApolloProvider>
   );
 }
 
